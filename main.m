67870abefea29620a8105a6dc137becedf53ba16
saberioon <saberioon@gmail.com>
2019-11-27 10:56:57 +0100

    first inint

---
+++ main.m
@@ -0,0 +1,89 @@
+clc 
+imtool close all 
+clear 
+captionFontSize = 14;
+
+im = imread ('TM0_0001.jpg');
+im = rgb2gray(im);
+
+[pixelCount, grayLevels] = imhist(im);
+
+bar(pixelCount);
+title('Histogram of original image', 'FontSize', captionFontSize);
+xlim([0 grayLevels(end)]); % Scale x axis manually.
+grid on;
+
+
+
+thresholdValue = 60;
+binaryImage = im < thresholdValue; % Bright objects will be chosen if you use >.
+imshow(binaryImage); subplot(2,1,1), imshow(im); subplot(2,1,2), imshow(binaryImage);
+
+binaryImage = imfill(binaryImage, 'holes');
+
+
+bar(pixelCount);
+title('Histogram of original image', 'FontSize', captionFontSize);
+xlim([0 grayLevels(end)]); % Scale x axis manually.
+grid on;
+hold on;
+maxYValue = ylim;
+line([thresholdValue, thresholdValue], maxYValue, 'Color', 'r');
+% Place a text label on the bar chart showing the threshold.
+annotationText = sprintf('Thresholded at %d gray levels', thresholdValue);
+% For text(), the x and y need to be of the data class "double" so let's cast both to double.
+text(double(thresholdValue + 5), double(0.5 * maxYValue(2)), annotationText, 'FontSize', 10, 'Color', [0 .5 0]);
+text(double(thresholdValue - 70), double(0.94 * maxYValue(2)), 'Background', 'FontSize', 10, 'Color', [0 0 .5]);
+text(double(thresholdValue + 50), double(0.94 * maxYValue(2)), 'Foreground', 'FontSize', 10, 'Color', [0 0 .5]);
+
+
+labeledImage = bwlabel(binaryImage, 8);
+imshow(labeledImage, []);  % Show the gray scale image.
+title('Labeled Image, from bwlabel()', 'FontSize', captionFontSize);
+
+
+coloredLabels = label2rgb (labeledImage, 'hsv', 'k', 'shuffle'); % pseudo random color labels
+imshow(coloredLabels);
+axis image; % Make sure image is not artificially stretched because of screen's aspect ratio.
+caption = sprintf('Pseudo colored labels, from label2rgb().\nBlobs are numbered from top to bottom, then from left to right.');
+title(caption, 'FontSize', captionFontSize);
+
+blobMeasurements = regionprops(labeledImage, im, 'all');
+numberOfBlobs = size(blobMeasurements, 1);
+
+
+imshow(im);
+title('Outlines, from bwboundaries()', 'FontSize', captionFontSize); 
+axis image; % Make sure image is not artificially stretched because of screen's aspect ratio.
+hold on;
+boundaries = bwboundaries(binaryImage);
+numberOfBoundaries = size(boundaries, 1);
+for k = 1 : numberOfBoundaries
+	thisBoundary = boundaries{k};
+	plot(thisBoundary(:,2), thisBoundary(:,1), 'g', 'LineWidth', 2);
+end
+hold off;
+
+
+textFontSize = 14;	% Used to control size of "blob number" labels put atop the image.
+labelShiftX = -7;	% Used to align the labels in the centers of the coins.
+
+blobECD = zeros(1, numberOfBlobs);
+% Print header line in the command window.
+fprintf(1,'Blob #      Mean Intensity  Area   Perimeter    Centroid       Diameter\n');
+% Loop over all blobs printing their measurements to the command window.
+for k = 1 : numberOfBlobs           % Loop through all blobs.
+	% Find the mean of each blob.  (R2008a has a better way where you can pass the original image
+	% directly into regionprops.  The way below works for all versions including earlier versions.)
+	thisBlobsPixels = blobMeasurements(k).PixelIdxList;  % Get list of pixels in current blob.
+	meanGL = mean(im(thisBlobsPixels)); % Find mean intensity (in original image!)
+	meanGL2008a = blobMeasurements(k).MeanIntensity; % Mean again, but only for version >= R2008a
+	
+	blobArea = blobMeasurements(k).Area;		% Get area.
+	blobPerimeter = blobMeasurements(k).Perimeter;		% Get perimeter.
+	blobCentroid = blobMeasurements(k).Centroid;		% Get centroid one at a time
+	blobECD(k) = sqrt(4 * blobArea / pi);					% Compute ECD - Equivalent Circular Diameter.
+	fprintf(1,'#%2d %17.1f %11.1f %8.1f %8.1f %8.1f % 8.1f\n', k, meanGL, blobArea, blobPerimeter, blobCentroid, blobECD(k));
+	% Put the "blob number" labels on the "boundaries" grayscale image.
+	text(blobCentroid(1) + labelShiftX, blobCentroid(2), num2str(k), 'FontSize', textFontSize, 'FontWeight', 'Bold');
+end
